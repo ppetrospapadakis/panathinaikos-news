e87794861f618ed5a47b714adb7d3948a6fbea00
ppetrospapadakis <ppetrospapadakis@users.noreply.github.com>
2026-08-06 16:07:53 +0300

    fix(news-sitemap): set req.query.type=news explicitly in wrapper function

--- api/news-sitemap.js
+++ api/news-sitemap.js
@@ -1,6 +1,8 @@
 const sitemapHandler = require('./sitemap.js');
 
 module.exports = async (req, res) => {
-    req.url = '/news-sitemap.xml';
+    req.query = req.query || {};
+    req.query.type = 'news';
+    req.url = '/sitemap.xml?type=news';
     return sitemapHandler(req, res);
 };
