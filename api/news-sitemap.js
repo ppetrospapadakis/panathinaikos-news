81608897371ce54b4de5befc9abb9a211361705c
ppetrospapadakis <ppetrospapadakis@users.noreply.github.com>
2026-08-06 16:08:32 +0300

    fix(sitemap): serve /news-sitemap.xml directly via /api/sitemap?type=news and remove broken lambda wrapper

--- api/news-sitemap.js
+++
@@ -1,8 +0,0 @@
-const sitemapHandler = require('./sitemap.js');
-
-module.exports = async (req, res) => {
-    req.query = req.query || {};
-    req.query.type = 'news';
-    req.url = '/sitemap.xml?type=news';
-    return sitemapHandler(req, res);
-};
